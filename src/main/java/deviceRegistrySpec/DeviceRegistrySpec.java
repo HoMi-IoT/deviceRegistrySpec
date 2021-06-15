0d25721e18fb278579810fe184708fff190e275d
QianMason <qianmason@gmail.com>
2021-06-15 14:34:29 -0700

    deviceregisty updated spec

--- src/main/java/deviceRegistrySpec/DeviceRegistrySpec.java
+++ src/main/java/deviceRegistrySpec/DeviceRegistrySpec.java
@@ -12,30 +12,34 @@ import java.util.List;
 import java.util.function.Predicate;
 
 class Types{
+	static TypeDef<String> deviceID = defineType(String.class, notNull());
+	static TypeDef<String> groupID = defineType(String.class, notNull());
 	static TypeDef<String> key = defineType(String.class, notNull());
 	static TypeDef<Object> value = defineType(Object.class, notNull());
+	static TypeDef<String[]> ids = defineType(String[].class, notNull());
 	static TypeDef<IObserver> observer = defineType(IObserver.class, notNull());
 	static Predicate<Object> fromBase = (o)->{
 		if (o.getClass().getModule().getName().equals("java.base") && o instanceof Serializable)
 			return true;
 		return false;
 	};
+
 }
 
 @SpecificationID(id = "DeviceRegistry") 
 public enum DeviceRegistrySpec implements ISpecification {
 	CREATEDEVICE(Boolean.class, defineSerializableType(Device.class, notNull())), // returns device ID, takes name and address
-	GETDEVICE(defineSerializableType(Device.class), String.class),
-	GETDEVICES(defineSerializableType(Device[].class, notNull()), String[].class),
+	GETDEVICE(defineSerializableType(Device.class), Types.deviceID), //deviceID
+	GETDEVICES(defineSerializableType(Device[].class, notNull()), Types.ids), // accepts array of deviceIDs
 	GETALLDEVICES(defineSerializableType(Device[].class)),
-	DELETEDEVICE(Boolean.class, String[].class), // takes deviceID's
-	SETATTRIBUTE(Boolean.class, String.class, String.class, defineType(Object.class, notNull(), Types.fromBase)), // takes deviceID, key, value
-	DELETEATTRIBUTE(Boolean.class, String.class, String.class), // takes devicename, attributekey
+	DELETEDEVICE(Boolean.class, Types.ids), // takes deviceID's
+	SETATTRIBUTE(Boolean.class, Types.deviceID, Types.key, defineType(Object.class, notNull(), Types.fromBase)), // takes deviceID, key, value
+	DELETEATTRIBUTE(Boolean.class, Types.deviceID, Types.key), // takes devicename, attributekey
 	//GETATTRIBUTE(Object.class, String.class), //keep or leave memories
-	ADDTOGROUP(Boolean.class, String.class, String.class), //deviceID, groupID
-	DELETEGROUP(Boolean.class, String.class), // takes groupID
-	DELETEFROMGROUP(Boolean.class, String.class, String.class),
-	GETGROUP(defineSerializableType(Device[].class), String.class); // takes deviceID's, and groupID
+	ADDTOGROUP(Boolean.class, Types.deviceID, Types.groupID), //deviceID, groupID
+	DELETEGROUP(Boolean.class, Types.groupID), // takes groupID
+	DELETEFROMGROUP(Boolean.class, Types.deviceID, Types.groupID),
+	GETGROUP(defineSerializableType(Device[].class), Types.groupID); // takes groupID
 	
 	
 	
